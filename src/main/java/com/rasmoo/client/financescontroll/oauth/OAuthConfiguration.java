3a6e5873c76e3e99c01e3927dd2a4c4affe1e741
FelipeDevRasmoo <felipe.alves@rasmoo.com>
2021-01-22 23:10:55 -0300

    Impl do grant type

--- src/main/java/com/rasmoo/client/financescontroll/oauth/OAuthConfiguration.java
+++ src/main/java/com/rasmoo/client/financescontroll/oauth/OAuthConfiguration.java
@@ -41,7 +41,7 @@ public class OAuthConfiguration {
 			.and()
 			.withClient("cliente-canva")
 			.secret("$2a$10$6zSKaHn71WVp8aP37Q2Ow.oFVCgNbHncEuhPmMWmIdKDFaBDoiVwG")
-			.authorizedGrantTypes("authorization_code,implicit")
+			.authorizedGrantTypes("authorization_code","implicit")
 			.scopes("read")
 			.redirectUris("https://www.canva.com/pt_br/")
 			.accessTokenValiditySeconds(3601)
